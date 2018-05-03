21d78e07546d3f1c5a2039fe4bcbf6904322cb60
Valentin Burkin <burkinman@me.com>
2018-05-03 12:29:58 +0300

    feat: Implement bookshelfs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import BookShelf from './BookShelf';
+import AddBook from './AddBook';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
 
 class App extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+      console.log(books);
+    })
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        <Route exact path='/' render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <BookShelf
+                shelfName='Currently Reading'
+                books={this.state.books.filter(b => b.shelf === 'currentlyReading')} />
+              <BookShelf
+                shelfName='Want To Read'
+                books={this.state.books.filter(b => b.shelf === 'wantToRead')} />
+              <BookShelf
+                shelfName='Read'
+                books={this.state.books.filter(b => b.shelf === 'read')} />
+            </div>
+          )}/>
+        <Route path='/search' render={({ history }) => (
+          <AddBook
+            onAddingBook={(book) => {
+              history.push('/')
+            }}
+          />
+        )}/>
+      </div>
     );
   }
 }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import Book from './Book';
 
 class BookShelf extends Component {
   render() {
+    const { shelfName, books } = this.props;
+
     return (
-      <div></div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfName}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map(book => (
+              <li key={book.id}>
+                <Book book={book} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
     );
   }
 }
